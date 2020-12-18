5f6ec97e31dc97cfa904ad17ca5d0981fb9e64e6
YangKeao <keao.yang@yahoo.com>
2020-12-18 16:48:17 +0800

    initial commit
    Signed-off-by: YangKeao <keao.yang@yahoo.com>

---
+++ update-cache.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 Chaos Mesh Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This script runs before all e2e jobs to prepare shared files.
+# This is optional. We only use it in CI to speed up our e2e process.
+#
+
+docker run --mount type=bind,source=$(pwd)/docker-cache,target=/var/lib/docker --privileged --env DOCKER_IN_DOCKER_ENABLED="true" --rm -it
+ --entrypoint runner.sh hub.pingcap.net/yangkeao/chaos-mesh-e2e-base /update-cache.sh 
